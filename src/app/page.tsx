5b8b08e037b6d4bf1780e8c583aeb745478648cd
PerpetuumG <perpetuumg@gmail.com>
2024-03-20 12:39:49 +0200

    initial project

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default async function Home() {
+  return (
+    <main className={'flex items-center justify-center py-20'}>
+      <p className={'text-xl font-semibold'}>hello</p>
+    </main>
+  );
+}
