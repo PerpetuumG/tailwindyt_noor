f1c71dbd374f0314b2fbd84d2251aa503a5357d9
PerpetuumG <perpetuumg@gmail.com>
2024-03-22 12:57:16 +0200

    add articles

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,9 +1,46 @@
+import SimpleLayout from '@/components/SimpleLayout';
+import { ArticleWithSlug, getAllArticles } from '@/lib/article';
+import { Card } from '@/components/Card';
+import { formatDate } from '@/lib/formatDate';
 
-const ArticlePage = () => {
+const Article = ({ article }: { article: ArticleWithSlug }) => {
   return (
-    <div>
+    <article className={'md:grid md:grid-cols-4 md:items-baseline'}>
+      <Card className={'md:col-span-3'}>
+        <Card.Title href={`/articles/${article.slug}`}>{article.title}</Card.Title>
 
-    </div>
+        <Card.Eyebrow as={'time'} dateTime={article.date} className={'md:hidden'} decorate>
+          {formatDate(article.date)}
+        </Card.Eyebrow>
+
+        <Card.Description>{article.description}</Card.Description>
+
+        <Card.Cta>Read article</Card.Cta>
+      </Card>
+
+      <Card.Eyebrow as={'time'} dateTime={article.date} className={'mt-1 hidden md:block'}>
+        {formatDate(article.date)}
+      </Card.Eyebrow>
+    </article>
+  );
+};
+
+const ArticlePage = async () => {
+  const articles = await getAllArticles();
+
+  return (
+    <SimpleLayout
+      title='Writing on software design, company building, and the aerospace industry.'
+      intro='All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order.'
+    >
+      <div className={'md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40'}>
+        <div className={'flex max-w-3xl flex-col space-y-16'}>
+          {articles.map(article => (
+            <Article key={article.slug} article={article} />
+          ))}
+        </div>
+      </div>
+    </SimpleLayout>
   );
 };
 
